dc63b252b20b2f23f5f8d9c88c0ec3fc4e9be21f
M. Enes Celik <m96celik@gmail.com>
2023-06-23 00:06:40 -0700

    local-db-adjusted

--- db.js
+++ db.js
@@ -1,28 +1,23 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-const password = process.env.DB_PASSWORD;
 
-// db connectionm
-mongoose.connect("mongodb+srv://m96celik:"+password+"@cluster0.2h20eno.mongodb.net/toDoListDB");
+mongoose.connect("mongodb://127.0.0.1:27017/toDoListDB");
 
-// item schema
+
 const itemsSchema = new mongoose.Schema({
     name: String
 });
 
 
-// list Schema
 const listSchema = new mongoose.Schema({
     name: String,
     items: [itemsSchema]
 });
 
 
-// item model 
 const Item = mongoose.model('Item', itemsSchema);
 
-// list model 
+
 const List = mongoose.model('List', listSchema);
 
 
